d3c8e826d8326609afc49ba22d1bb8e141146456
sunsetjkyle <sunsetkyle@protonmail.com>
2022-07-05 11:47:59 +0300

    Declare and assign variables in js

---
+++ hello.js
@@ -0,0 +1,45 @@
+//This is the beginning of my JavaScript code -inline comment
+/* This is a multi line 
+comment, looks similiar like the C commenting style*/
+/* JavaScript has seven data types, namely; 
+Undefined - not defined yet 
+null- nothing, 
+boolean-true or false, 
+String- text
+Symbol - unique value
+number  - number
+object - store key value pairs
+
+variables allow computers to store and manipulate data
+*/
+//how to declare variables in js
+var myName = "Kyle";
+//var used for the whole program
+myName = 8;
+// way 2
+let ourName = "Sunset Jesus"
+//let is used within the scope you declare
+//way 3
+const g = 9.8
+//const does not change
+//declaring and assigning a variable
+var z;
+console.log(z);
+z = 21;
+//equal sign is thr assignment operator
+var y;
+y = 17;
+z=y
+console.log(z);
+console.log(y);
+//initialising and declaring
+var x =2;
+var n = 3;
+var q = "I am a ";
+
+x = x+1;
+n = n+5;
+q = q + "String";
+console.log(x,n,q);
+
+//declaration is case sensitive, use camel case
