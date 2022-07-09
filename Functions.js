b7485239ae6ce0bfd3ba39a0f73f868577e8ebd8
sunsetjkyle <sunsetkyle@protonmail.com>
2022-07-09 13:43:53 +0300

    Functions in js

---
+++ Functions.js
@@ -0,0 +1,18 @@
+//function is a re-usable code in js
+function ourReusableFunction() {
+    console.log("Sunset Jesus");
+    
+}
+
+ourReusableFunction();
+ourReusableFunction();
+ourReusableFunction();
+ourReusableFunction();
+
+function postMyNumber() {
+    console.log("My number is 0123456789");
+    
+}
+
+postMyNumber();
+postMyNumber();
