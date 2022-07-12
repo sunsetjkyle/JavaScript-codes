efbe0bb793deb6c33b777dbd6ebf2459af10065a
sunsetjkyle <sunsetkyle@protonmail.com>
2022-07-13 01:17:59 +0300

    how to console.log booleans

--- Booleans.js
+++ Booleans.js
@@ -3,4 +3,22 @@ function welcomeToBooleans() {
     return false;
     
 }
+//use of conditional statements using if
+function Ronaldo(isTheGoat) {
+    if (isTheGoat) {
+        return "Yes, he is de goat";
+        
+    }
+    return "No, es ist falsch";
+    
+}
+console.log(Ronaldo(false));
 
+function isItTrue(FalschOderRichtig) {
+    if (FalschOderRichtig) {
+        return "Ja, es war richtig";
+    }
+    return "es war nicht richtig";
+    
+}
+console.log(isItTrue(true));
